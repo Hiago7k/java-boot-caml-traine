6af6fa9d9d43794c7444593d28946270033c3c2b
Hiago 7k <hiago7k@gmail.com>
2026-06-15 18:48:18 -0300

    praticando calculadora idade

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        Scanner dadosNumericos = new Scanner(System.in);
+        Scanner dados = new Scanner(System.in);
+        System.out.println("Digite o ano atual que estamos: ");
+        int anoAtual = dados.nextInt();
+        System.out.println("Digite o ano que você nasceu: ");
+        int anoNascimento = dados.nextInt();
 
-       /* System.out.println("Bem vindo a loja de bebidas, por gentileza digite sua idade: ");
-        Integer idade = dadosNumericos.nextInt();
-        // transformar numerico int em string
-        String valorConvertido = Integer.toString(idade);
-        System.out.println(valorConvertido);
-
-        */
-
-
-        // Transformar String em INT
-        System.out.println("Digite seu ano: ");
-        String anoDeNascimento = dadosNumericos.nextLine();
-        int valorInteiro = Integer.parseInt(anoDeNascimento);
-
-        System.out.println(valorInteiro + 1);
+        int resultado = anoAtual - anoNascimento;
+        System.out.println("Você tem a idade de  " + resultado);
+        
     }
 }
