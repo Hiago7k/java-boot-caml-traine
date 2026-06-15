d936f9b94401c5673fd3e4f73fc7968230199688
Hiago 7k <hiago7k@gmail.com>
2026-06-15 18:00:21 -0300

    vendo converssões de tipos

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        System.out.println("Ola seja bem vindo, a Loteria digite o nome do boleto");
-        Scanner informacoes = new Scanner(System.in);
-        String nomeDoBoleto = informacoes.nextLine();
+        Scanner dadosNumericos = new Scanner(System.in);
 
-        System.out.println("Salvando dados, do boleto no nosso banco de dados...");
-        System.out.println("O boleto foi salvo codigo do boleto " + nomeDoBoleto);
+       /* System.out.println("Bem vindo a loja de bebidas, por gentileza digite sua idade: ");
+        Integer idade = dadosNumericos.nextInt();
+        // transformar numerico int em string
+        String valorConvertido = Integer.toString(idade);
+        System.out.println(valorConvertido);
+
+        */
+
+
+        // Transformar String em INT
+        System.out.println("Digite seu ano: ");
+        String anoDeNascimento = dadosNumericos.nextLine();
+        int valorInteiro = Integer.parseInt(anoDeNascimento);
+
+        System.out.println(valorInteiro + 1);
     }
 }
