a474cb3044cf7f232b45f8bb3adbf13f33e3117e
Hiago 7k <hiago7k@gmail.com>
2026-06-15 20:04:53 -0300

    operadores e arrendondamentos

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,28 @@ class Main {
     public static void main(String[] args)
     {
 
+//        int numero = 10;
+//        int valor = 4 + numero--;
+//        System.out.println(valor); // 14 pos incremento
+//        System.out.println(numero); // 9
 
-        int numero = 10;
-        int valor = 4 + numero--;
-        System.out.println(valor); // 14 pos incremento
-        System.out.println(numero); // 9
+        //Operadoras de Atribuicao
+//
+//       int x = 4;
+//       x += 2; // x = x + 2
+//        System.out.println(x);
+
+
+        // Arrendondamento utilizando Math
+        // ceil arrendondando por artimetica
+        double numero = 5.6;
+        double numeroArrendondado = Math.ceil(numero);
+        System.out.println(numeroArrendondado);
+
+        // floor arrendondando para baixo
+        float numerof = 8.9f;
+        int numeroArrendodadoF = (int) Math.floor(numerof); // forcando o tipo
+        System.out.println(numeroArrendodadoF); // saida esperada 8
 
     }
 }
