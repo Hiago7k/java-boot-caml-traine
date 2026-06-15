4f037e94ad445f35a4b0c07f493a0b9b76cb169a
Hiago 7k <hiago7k@gmail.com>
2026-06-15 17:46:04 -0300

    estudando sobre scanner, tipo de entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World Java!");
+        System.out.println("Ola seja bem vindo, a Loteria digite o nome do boleto");
+        Scanner informacoes = new Scanner(System.in);
+        String nomeDoBoleto = informacoes.nextLine();
+
+        System.out.println("Salvando dados, do boleto no nosso banco de dados...");
+        System.out.println("O boleto foi salvo codigo do boleto " + nomeDoBoleto);
     }
 }
