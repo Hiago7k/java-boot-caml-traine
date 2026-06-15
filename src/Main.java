e2843834779c66ef7ea8c4a2c45176d99fd3f496
Hiago 7k <hiago7k@gmail.com>
2026-06-15 18:51:20 -0300

    pegando ano do sistema

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ class Main {
     public static void main(String[] args)
     {
         Scanner dados = new Scanner(System.in);
-        System.out.println("Digite o ano atual que estamos: ");
-        int anoAtual = dados.nextInt();
-        System.out.println("Digite o ano que você nasceu: ");
+        System.out.println("=====================================");
+        System.out.println("Calculador idade");
+        System.out.println("=====================================");
+        System.out.println("Digite o ano de nascimento: ");
         int anoNascimento = dados.nextInt();
 
+        int anoAtual = java.time.Year.now().getValue();
+
         int resultado = anoAtual - anoNascimento;
-        System.out.println("Você tem a idade de  " + resultado);
-        
+        System.out.println("Você tem " + resultado + " anos");
     }
 }
