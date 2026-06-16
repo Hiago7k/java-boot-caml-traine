ec8478ae845bf59a3f5f213d5e8358ccfa641711
Hiago 7k <hiago7k@gmail.com>
2026-06-16 20:10:00 -0300

    testando operadores logicos

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-       // Comparando Strings
-        String nome1 = "Hiago";
-        String nome2 = "Hiago";
-        String nome3 = new String("Hiago");
-        String res;
-        res = (nome1.equals(nome3))?"igual":"diferente";
-        //a variavel res vai receber ou a palavra igual ou a palavra diferente
-        System.out.println(res);
+       // Sistema de votação pessoas de 16 a menor de 18 podem votar opcionalmente
+        // pessoas de 18 até 70 sao obrigadas a votar
+        // pessomas acima de 70 votam opcionalmente
+
+        int idade = 19;
+        String eleitor;
+
+        eleitor = (idade >= 16 && idade < 18 || idade >= 70)?"pode votar se quiser":"voto obrigatorio";
+        System.out.println(eleitor);
     }
 }
