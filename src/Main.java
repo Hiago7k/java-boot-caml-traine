023e66ee79f2668c05054bd650890c994f52dcd5
Hiago 7k <hiago7k@gmail.com>
2026-06-16 19:05:03 -0300

    operador termerario

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        System.out.println("**************************");
-        System.out.println("-------Calculadora--------");
-        System.out.println("-------- + - = x ---------");
-        System.out.println("**************************");
-        System.out.print("Informe um valor: ");
-        Scanner numeroRecebido = new Scanner(System.in);
-        float numeroDaCalculadora = numeroRecebido.nextFloat();
+       // Operador Ternario
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 8;
+        r = (n1>n2)?n1:n2;
+        System.out.println(r);
 
-        int restoDivisao = (int) numeroDaCalculadora % 2;
-        float raizQuadrada = (float) Math.sqrt(numeroDaCalculadora);
-        float raizCubica = (float) Math.cbrt(numeroDaCalculadora);
-        float elevadoAocubo = (float) Math.pow(numeroDaCalculadora,3);
-        float valorAbsoluto =  (float) Math.abs(-numeroDaCalculadora);
-
-        System.out.println("Resto da divisão por 2 é: " + restoDivisao);
-        System.out.println("Raiz Quadrada: " + raizQuadrada);
-        System.out.println("Raiz cúbica: " + raizCubica);
-        System.out.println("Elevado ao cubo: " + elevadoAocubo);
-        System.out.println("Valor absoluto: " + valorAbsoluto);
     }
 }
