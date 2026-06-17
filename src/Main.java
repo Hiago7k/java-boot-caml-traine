1f740ba284960cac273161e6cd7960c9b7630713
Hiago 7k <hiago7k@gmail.com>
2026-06-17 19:58:28 -0300

    estruturas condicionais

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-      // Adivinhando um Numero AkiNumber
-        System.out.println("*******************************");
-        System.out.println("--- Bem vindo ao AkiNumber ---");
-        System.out.println("*******************************");
+      // Estruturas Condicionais
 
-        System.out.println("Tente advinhar o número que estou pensando de 1 a 5 digite: ");
-        Scanner dados = new Scanner(System.in);
-        int numeroDoUsuario = dados.nextInt();
-        System.out.println(numeroDoUsuario);
+        float n1, n2;
+        n1 = 1;
+        n2  = 8;
+        float m = (n1 + n2) / 2;
 
-        double numeroSecreto = Math.random();
-        int num = (int) (1 + numeroSecreto * (5-1));
-        
-        String resposta;
+        if(m >= 6){
+            System.out.println("Parabens passou de ano");
+            System.out.println("com a media " + m);
+        } else{
+            System.out.println("Reprovado");
+        }
 
-        resposta = (num==numeroDoUsuario)?"Voce acertou o numero":"Voce errou o numero";
-        System.out.println("O numero que pensei foi " + num);
-        System.out.println(resposta);
+
     }
 }
