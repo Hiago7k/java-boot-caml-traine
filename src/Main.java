6f6b7ba1d7fb2a9a57cf7c7aa7d0fd5a011e1f6d
Hiago 7k <hiago7k@gmail.com>
2026-06-18 18:34:26 -0300

    if

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,17 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-      // Estruturas Condicionais
-        System.out.println("********************************");
-        System.out.println("Resultado escolar final de ano");
-        System.out.println("********************************");
         Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite sua primeira nota: ");
+        float n1 = teclado.nextFloat();
 
-        System.out.println("Digite a media do primeiro semestre: ");
-        float notaPrimeiroSemestre = teclado.nextFloat();
-        System.out.println("Digite a media do segundo semestre: ");
-        float notaSegundoSemestre = teclado.nextFloat();
-        float media = (notaPrimeiroSemestre + notaSegundoSemestre) / 2;
+        System.out.print("Digite sua segunda nota: ");
+        float n2 = teclado.nextFloat();
+        float m =  (n1 + n2) / 2;
 
-        if (media >= 7)
-        {
-            System.out.println("Aprovado media: " + media);
-        } else if (media >= 5 && media <= 6) {
-            System.out.println("Recuperação media: " + media);
-        } else
-        {
-            System.out.println("Reprovado media: " + media);
+        System.out.println("Sua média é " + m);
+        if(m > 9) {
+            System.out.println("Parabens pela sua nota " + m);
         }
-
     }
 }
