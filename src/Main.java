f49ec3635d664aa837e59cf6cc077ee2f396eda3
Hiago 7k <hiago7k@gmail.com>
2026-06-22 20:59:36 -0300

    triangulos

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
+        System.out.println("Tipos de traingulo");
         Scanner data = new Scanner(System.in);
-        System.out.println("Digite a quantidade de pernas: ");
-        int pernas = data.nextInt();
-        String tipo;
+        System.out.println("Segmento A do triangulo: ");
+        int a = data.nextInt();
+        System.out.println("Segmento B do triangulo: ");
+        int b = data.nextInt();
+        System.out.println("Segmento C do triangulo: ");
+        int c = data.nextInt();
 
-        switch (pernas){
-            case 1:
-                tipo = "Saci";
-                break;
-            case 2:
-                tipo = "Bipedi";
-                break;
-            case 4:
-                tipo = "Quadrupide";
-                break;
-            case 6, 8:
-                tipo = "Aranha";
-                break;
-            default:
-                tipo = "Et";
-                break;
+
+        if(a<b+c && b<a+c  && c<a+b){
+            if (a ==b && b==c)
+            {
+                System.out.println("Equilatero");
+            }else if(a!=b && b!=c && a!=c){
+                System.out.println("Escaleno");
+            }else {
+                System.out.println("Isoselis");
+            }
+        }else {
+            System.out.println("Não forma um triangulo");
+            System.out.println("-------");
         }
-        System.out.println(tipo);
 
     }
 }
