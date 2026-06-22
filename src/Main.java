2a378a7dc67df45bb10987cca719011fc8db89af
Hiago 7k <hiago7k@gmail.com>
2026-06-22 19:51:48 -0300

    switch em java

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,28 @@ class Main {
     public static void main(String[] args)
     {
         Scanner data = new Scanner(System.in);
-        System.out.println("Digite o ano que voces naceu: ");
-        int nasc = data.nextInt();
-        int idade = 2026 - nasc;
+        System.out.println("Digite a quantidade de pernas: ");
+        int pernas = data.nextInt();
+        String tipo;
 
-        if(idade < 16){
-            System.out.println("Não pode votar ainda pois voce tem " + idade + " anos");
-        } else if ((idade >= 16 && idade < 18) || (idade > 70)) {
-            System.out.println("Pode votar se quiser pois possui " + idade + " anos");
-        }else {
-            System.out.println("Voto obrigatorio pois sua iade é " + idade + " anos");
+        switch (pernas){
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bipedi";
+                break;
+            case 4:
+                tipo = "Quadrupide";
+                break;
+            case 6, 8:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "Et";
+                break;
         }
+        System.out.println(tipo);
+
     }
 }
