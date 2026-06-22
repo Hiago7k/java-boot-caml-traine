41962ee36c1f2c28e365f4e8bf575b3b3625a6e3
Hiago 7k <hiago7k@gmail.com>
2026-06-22 19:36:13 -0300

    utilizando else if

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,12 @@ class Main {
         int nasc = data.nextInt();
         int idade = 2026 - nasc;
 
-        if (idade < 16)
-        {
-            System.out.println("Não pode votar " + idade);
+        if(idade < 16){
+            System.out.println("Não pode votar ainda pois voce tem " + idade + " anos");
+        } else if ((idade >= 16 && idade < 18) || (idade > 70)) {
+            System.out.println("Pode votar se quiser pois possui " + idade + " anos");
         }else {
-            if ((idade >= 16 && idade < 18) || (idade > 70))
-            {
-                System.out.println("Voto opcional " + idade + " anos");
-            }else {
-                System.out.println("Voto obrigatorio " + idade  + " anos");
-            }
+            System.out.println("Voto obrigatorio pois sua iade é " + idade + " anos");
         }
     }
 }
