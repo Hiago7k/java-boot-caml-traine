8b02f78b0deb12b67bcfb29d8ffd8752bcb03383
Hiago 7k <hiago7k@gmail.com>
2026-06-23 20:21:15 -0300

    fatorial

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,15 @@ class Main {
     public static void main(String[] args)
     {
         Scanner data = new Scanner(System.in);
-        // calculo de fatorial
-        // 4 x 3 x 2 x 1
-        // ou seja o fatorial é o numero que escolhemos 10
-        // ai fazemos o 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 1
-        // o resultado final seria 5 x 4 x 3 x 2 x 1 = 120
-        // 120 seria o fatorial
 
-
-
-        int i = 1;
-        int a = 0;
-
-        while (i < 10){
-            ++a; // 10 vezes o a vira 11
-            i = i + 1; // 2
-            if (a> 6) i++; // essa espressao vai ser true apenas 4 vezes 2 6 8
-        }
-        System.out.println(i); // aqui é 11
-        System.out.println(a); // aqui é 8
-
-
-
-
-
+        int n = 5;
+        int f = 1;
+        int c = n;
+      while(c >= 1)
+      {
+          f *= c;
+          c--;
+      }
+        System.out.println(f);
     }
 }
