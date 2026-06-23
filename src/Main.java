84c0ba6fb5aec24cf5911cafcf7dba2f80e7b2f2
Hiago 7k <hiago7k@gmail.com>
2026-06-23 17:10:17 -0300

    utilizando break

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ class Main {
         int cambalhota = 0;
         while (cambalhota < 10){
             cambalhota++ ;
-            if (cambalhota == 5 || cambalhota == 7 || cambalhota == 9) {
+            if (cambalhota == 2 || cambalhota == 3 || cambalhota == 4) {
                 continue;
+            }
+
+            if(cambalhota == 7){
+                break;
             }
             System.out.println("Dando uma cambolha pela vez: " + cambalhota);
         }
