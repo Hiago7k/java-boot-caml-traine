b6f2c301f9c8dba1dcfa06b6126214a271d32eb8
Hiago 7k <hiago7k@gmail.com>
2026-06-23 16:05:55 -0300

    lacos de repeticao

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,11 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        System.out.println("Tipos de traingulo");
-        Scanner data = new Scanner(System.in);
-        System.out.println("Segmento A do triangulo: ");
-        int a = data.nextInt();
-        System.out.println("Segmento B do triangulo: ");
-        int b = data.nextInt();
-        System.out.println("Segmento C do triangulo: ");
-        int c = data.nextInt();
-
-
-        if(a<b+c && b<a+c  && c<a+b){
-            if (a ==b && b==c)
-            {
-                System.out.println("Equilatero");
-            }else if(a!=b && b!=c && a!=c){
-                System.out.println("Escaleno");
-            }else {
-                System.out.println("Isoselis");
-            }
-        }else {
-            System.out.println("Não forma um triangulo");
-            System.out.println("-------");
+        // Lacos de repeticao Enquanto
+        int cambalhota = 0;
+        while (cambalhota < 4){
+            cambalhota++ ;
+            System.out.println("Dando uma cambolha pela vez: " + cambalhota);
         }
-
     }
 }
