5243f94f872e92920f6764ac63e16a2c52cedb87
Hiago 7k <hiago7k@gmail.com>
2026-06-23 17:05:57 -0300

    usando continue para interror fluxo natural

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        // Lacos de repeticao Enquanto
         int cambalhota = 0;
-        while (cambalhota < 4){
+        while (cambalhota < 10){
             cambalhota++ ;
+            if (cambalhota == 5 || cambalhota == 7 || cambalhota == 9) {
+                continue;
+            }
             System.out.println("Dando uma cambolha pela vez: " + cambalhota);
         }
     }
