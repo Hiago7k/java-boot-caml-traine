1099706f17639296d9758a8e986352ba559892a9
Hiago 7k <hiago7k@gmail.com>
2026-06-24 18:05:44 -0300

    prttice for

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class Main {
     public static void main(String[] args)
     {
        // Lacos de repeticao III
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 0; i <= 100; i+=10) {
             System.out.println(i);
         }
     }
