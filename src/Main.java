90ce92ab064a9eecff5861f1ecdeee45b96ab48c
Hiago 7k <hiago7k@gmail.com>
2026-06-24 18:02:36 -0300

    laco for

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        Scanner data =  new Scanner(System.in);
-        int n, s=0, p=0, i=0, h=0;
-        float media = 0;
-        do {
-            System.out.println("Informe um numero: " + " Valor 0 interrompe");
-            n = data.nextInt();
-            s += n;
-
-            if(n != 0){
-               media = (float) s / n;
-            }
-
-            if(n % 2 == 0 && n > 0){
-              p++;
-              if (n > 100) h++;
-            } else if(n > 0) {
-                i++;
-            }
-        }while (n != 0);
-        System.out.println("--- Resultado ----");
-        System.out.println("Total de valores: " + s);
-        System.out.println("Total de pares: " + p);
-        System.out.println("Total de impares: " + i);
-        System.out.println("Acima de 100: " + h);
-        System.out.println("Media dos valores: " + media);
-        System.out.println("------------");
+       // Lacos de repeticao III
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(i);
+        }
     }
 }
