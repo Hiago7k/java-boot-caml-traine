4dc71a6e46a398aa1e034c300a39f48aa20e057e
Hiago 7k <hiago7k@gmail.com>
2026-06-25 19:00:27 -0300

    metodo de find

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.sql.Array;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args)
     {
-        //ForEach para Cada
-        int nums[] = {1,2,3,45,123};
-        //ForEach
+        int nums[] = {1, 2 ,3 ,4 ,5 ,6};
         for(int valor: nums){
-            System.out.println(valor);
+            System.out.print(valor + "");
         }
+        System.out.println();
+        int posicao = Arrays.binarySearch(nums, 1);
+        System.out.println("Encontrei o valor na posicao " + posicao);
+
     }
 }
