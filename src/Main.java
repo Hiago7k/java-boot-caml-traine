17d01683f650381683154fde6f39bee085bf8aff
Hiago 7k <hiago7k@gmail.com>
2026-06-25 19:07:25 -0300

    array com auto incremento

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,11 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        int nums[] = {1, 2 ,3 ,4 ,5 ,6};
-        for(int valor: nums){
-            System.out.print(valor + "");
-        }
-        System.out.println();
-        int posicao = Arrays.binarySearch(nums, 1);
-        System.out.println("Encontrei o valor na posicao " + posicao);
+        int nums[] = new int[20];
+        Arrays.fill(nums, 10);
+       for(int valor: nums){
+           System.out.println(valor);
+       }
 
     }
 }
