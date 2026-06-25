0f78c1ccbc37d938270a563a2d0071830a9d14ca
Hiago 7k <hiago7k@gmail.com>
2026-06-25 17:56:49 -0300

    vetor em java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-     // laco de repeticao para contar de 1 a 50
-        for (int i = 1; i <= 5; i++){
-            for (int j = i +1; j <=5; j++)
-            {
-                System.out.print(i);
-                System.out.println(j);
-            }
-        }
+        // Vetores Variaveis composta
+
+      int n[] = {1,2,3,4};
+
+
     }
 }
