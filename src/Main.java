342c8e02c3263f2b9fdde91dcf8d54ccb9bedacb
Hiago 7k <hiago7k@gmail.com>
2026-06-25 18:15:18 -0300

    praticando

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        // Vetores Variaveis composta
-            int n[] = {10,21,33,42};
-            for(int c = 0; c<= n.length-1; c++){
-                System.out.println(n[c]);
-            }
+       String mes[] = {"janeiro", "fevereiro", "março", "abril", "maio",
+               "junho", "julho", "agosto", "setembro", "outrubro",
+               "novembro", "dezembro"
+       };
 
+       int diasMes[] = {31,28,31,30,30,31,31,30,30,31,30,31};
+
+       for(int i = 0; i <=mes.length-1; i++){
+           System.out.println(mes[i] + "tem "  + diasMes[i] + " dias");
+       }
     }
 }
