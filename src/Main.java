931443b9bbf2ff041d731321b07be4a69acf85d6
Hiago 7k <hiago7k@gmail.com>
2026-06-25 18:49:45 -0300

    foreach em java

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-       String mes[] = {"janeiro", "fevereiro", "março", "abril", "maio",
-               "junho", "julho", "agosto", "setembro", "outrubro",
-               "novembro", "dezembro"
-       };
-
-       int diasMes[] = {31,28,31,30,30,31,31,30,30,31,30,31};
-
-       for(int i = 0; i <=mes.length-1; i++){
-           System.out.println(mes[i] + "tem "  + diasMes[i] + " dias");
-       }
+        //ForEach para Cada
+        int nums[] = {1,2,3,45,123};
+        //ForEach
+        for(int valor: nums){
+            System.out.println(valor);
+        }
     }
 }
