becfdd299a52bbdd30fdd83fd96d0972b82366af
Hiago 7k <hiago7k@gmail.com>
2026-06-30 16:43:04 -0300

    métodos em java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,20 @@
+import javax.swing.*;
 import java.sql.Array;
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args)
-    {
-        // Criando um array
-        int numeros[] = {10, 232, 85, 754, 2323, 0};
-        // iterando sobre o array com foreach
-        System.out.println("Utilizando foreach");
-        for (var valores : numeros ){
-            System.out.println(valores);
-        }
 
-        // iterando sobre o array com for
-        System.out.println();
-        System.out.println("Utilizando for");
-        for(int i = 0; i <= numeros.length -1; i ++)
-        {
-            System.out.println(numeros[i]);
-        }
+    static void soma (int a, int b){
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+        System.out.println("Método que criei e executado depois");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Método Main é executado primeiro");
+        soma(10,2);
     }
 }
