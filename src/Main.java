7f4e405e63325ffc15baeff36b4b9c7d5d9e8c4e
Hiago 7k <hiago7k@gmail.com>
2026-06-30 16:58:08 -0300

    metodos e classes

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 class Main {
-
-
-    // Método sem retorno
-    static void soma (int a, int b){
-        int soma = a + b;
-        System.out.println("A soma é: " + soma);
-    }
-
-    static int somaComRetorno (int a, int b){
-        int soma = a + b;
-        return soma;
-    }
-
     public static void main(String[] args) {
-        soma(10,2); // soma sem retonro
-        int resposta = somaComRetorno(10,50);
-        System.out.println(resposta);
+        System.out.println("Vai comecar a contagem");
+        System.out.println(operacoes.contador(1,5));
     }
 }
